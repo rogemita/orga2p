4647f0e8cfb4cc07d3f305e32579d68b2a0f3752
RogelioMita <RogelioMita@2de975fa-a958-11de-9787-55e617d4dd53>
2009-09-28 20:20:57 +0000

    Aqui esta la revision lista para arrancar la funcion de asm

--- trunk/tp1-a/implementacion/src/bordes.c
+++ trunk/tp1-a/implementacion/src/bordes.c
@@ -3,37 +3,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+extern void asmSobel(IplImage * src, IplImage * dst, int ancho, int alto, int xorder, int yorder);
 
 int main( int argc, char** argv )
 {
 
-   IplImage * src = 0;
-   IplImage * dst = 0;
-   IplImage * dst_ini = 0;
-
-   char* filename = argc == 2 ? argv[1] : (char*)"lena.bmp";
-
-   // Cargo la imagen
-   if( (src = cvLoadImage (filename, CV_LOAD_IMAGE_GRAYSCALE)) == 0 )
-	   return -1;
-
-   // Creo una IplImage para cada salida esperada
-   if( (dst = cvCreateImage (cvGetSize (src), IPL_DEPTH_8U, 1) ) == 0 )
-	   return -1;
-
-   // Creo una IplImage para cada salida esperada
-   if( (dst_ini = cvCreateImage (cvGetSize (src), IPL_DEPTH_8U, 1) ) == 0 )
-	   return -1;
-
-   // Aplico el filtro (Sobel con derivada x en este caso) y salvo imagen 
-   cvSobel(src, dst, 1,0,3); 	// Esta parte es la que tienen que programar los alumnos en ASM	y comparar
-   cvSaveImage("derivada x.BMP", dst);
-
-   // Aplico el filtro (Sobel con derivada y en esta caso) y salvo imagen 
-   cvSobel(src, dst, 0,1,3);    // Esta parte es la que tienen que programar los alumnos en ASM	y comparar
-   cvSaveImage("derivada y.BMP", dst);
-
-   return 0;
+	IplImage * src = 0;
+	IplImage * dst = 0;
+	IplImage * dst_asm = 0;
+	IplImage * dst_ini = 0;
+	
+	char* filename = argc == 2 ? argv[1] : (char*)"img/lena.bmp";
+	
+	// Cargo la imagen
+	if( (src = cvLoadImage (filename, CV_LOAD_IMAGE_GRAYSCALE)) == 0 )
+		return -1;
+	
+	// Creo una IplImage para cada salida esperada
+	if( (dst = cvCreateImage (cvGetSize (src), IPL_DEPTH_8U, 1) ) == 0 )
+		return -1;
+	
+	// Creo una IplImage para cada salida esperada
+	if( (dst_ini = cvCreateImage (cvGetSize (src), IPL_DEPTH_8U, 1) ) == 0 )
+		return -1;
+	
+	// Creo una IplImage para la salida de la funcion en asembler
+	if( (dst_asm = cvCreateImage (cvGetSize (src), IPL_DEPTH_8U, 1) ) == 0 )
+		return -1;
+	
+	// Aplico el filtro (Sobel con derivada x en este caso) y salvo imagen 
+	cvSobel(src, dst, 1,0,3); 	// Esta parte es la que tienen que programar los alumnos en ASM	y comparar
+	asmSobel(src, dst_asm, cvGetSize (src).width, cvGetSize(src).height, 1, 0);
+	cvSaveImage("img/derivada x.BMP", dst);
+	
+	// Aplico el filtro (Sobel con derivada y en esta caso) y salvo imagen 
+	cvSobel(src, dst, 0,1,3);    // Esta parte es la que tienen que programar los alumnos en ASM	y comparar
+	asmSobel(src, dst_asm, cvGetSize (src).width, cvGetSize(src).height, 0, 1);
+	cvSaveImage("img/derivada y.BMP", dst);
+	
+	return 0;
 
 }
 
