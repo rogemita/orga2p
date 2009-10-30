1c5ee61e2360e3d735693c9a6c272c42e599551d
vscorza <vscorza@2de975fa-a958-11de-9787-55e617d4dd53>
2009-10-30 05:43:33 +0000

    ya esta casi listo el primero, esta metiendo seis pixeles mal al final de cada linea, mmmmm

--- tp1-b/implementacion/src/bordes.c
+++ tp1-b/implementacion/src/bordes.c
@@ -12,7 +12,8 @@ int main( int argc, char** argv )
    IplImage * dst = 0;
    IplImage * dst_ini = 0;
 
-   char* filename = argc == 2 ? argv[1] : (char*)"img/in/lena-full.jpg";
+   char* filename = argc == 2 ? argv[1] : (char*)"img/in/lena.bmp";
+   //char* filename = argc == 2 ? argv[1] : (char*)"img/in/lena-full.jpg";
 
    // Cargo la imagen
    if( (src = cvLoadImage (filename, CV_LOAD_IMAGE_GRAYSCALE)) == 0 )
